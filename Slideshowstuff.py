6a2c07f558cabf4fffc27460624dc098e278e5c9
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-30 21:50:34 +0100

    Update Slideshowstuff.py

--- Slideshowstuff.py
+++ Slideshowstuff.py
@@ -13,49 +13,48 @@ import RPi.GPIO as GPio, time, os, shlex
 # "Sep":"09",  "Oct":"10", "Nov": "11", "Dec":"12"}
 # adjust window
 win=tk.Tk()
-  
+def run():
 # loading the images
-
-screen_width = win.winfo_screenwidth()
-screen_height = win.winfo_screenheight()
-win.geometry(str(screen_width)+"x"+str(screen_height))
+    screen_width = win.winfo_screenwidth()
+    screen_height = win.winfo_screenheight()
+    win.geometry(str(screen_width)+"x"+str(screen_height))
 #####
 #resize
 #######
 #find divisor
-path = "/home/pi64/Slideshow"
+    path = "/home/pi64/Slideshow"
 #path = input("Please give the directory location of your files")
-print("Resizing photos...")
-dir_list = os.listdir(path)
-imgarray = []
-for i in range(0,len(dir_list)):
-    if dir_list[i][-3:]=="jpg" or dir_list[i][-3:]=="JPG" or dir_list[i][-3:]=="PNG" or dir_list[i][-3:]=="png":
-        img = Image.open(str(dir_list[i]))
-        w,h = img.size
-        ratio = h/w
-        if ratio > 1:
-            resized_image = img.resize((int(screen_width),int(ratio * screen_width)))
-        else:
-            ratio = w/h
-            resized_image = img.resize((int(screen_height * ratio),int(screen_height)))
-        img = ImageTk.PhotoImage(resized_image)
-        imgarray.append(img)
+    print("Resizing photos...")
+    dir_list = os.listdir(path)
+    imgarray = []
+    for i in range(0,len(dir_list)):
+        if dir_list[i][-3:]=="jpg" or dir_list[i][-3:]=="JPG" or dir_list[i][-3:]=="PNG" or dir_list[i][-3:]=="png":
+            img = Image.open(str(dir_list[i]))
+            w,h = img.size
+            ratio = h/w
+            if ratio > 1:
+                resized_image = img.resize((int(screen_width),int(ratio * screen_width)))
+            else:
+                ratio = w/h
+                resized_image = img.resize((int(screen_height * ratio),int(screen_height)))
+            img = ImageTk.PhotoImage(resized_image)
+            imgarray.append(img)
 #check current date
-try:
-    modified_time = os.path.getmtime(path+"/"+dir_list[0])
-    convert_time = time.ctime(modified_time)
-    current_time = datetime.datetime.now() 
-    array = convert_time.split(" ")
-    timearray = array[3].split(":")
-    year = int(array[4])
-    month = int(monthdic[array[1]])
-    day = int(array[2])
-    hour = int(timearray[0])
-    minute = int(timearray[1])
-    second = int(timearray[2])
-    current_time = str(current_time)
-    timearray = current_time.split("-")
-    timearray[1] = int(timearray[1])
+    try:
+        modified_time = os.path.getmtime(path+"/"+dir_list[0])
+        convert_time = time.ctime(modified_time)
+        current_time = datetime.datetime.now() 
+        array = convert_time.split(" ")
+        timearray = array[3].split(":")
+        year = int(array[4])
+        month = int(monthdic[array[1]])
+        day = int(array[2])
+        hour = int(timearray[0])
+        minute = int(timearray[1])
+        second = int(timearray[2])
+        current_time = str(current_time)
+        timearray = current_time.split("-")
+        timearray[1] = int(timearray[1])
     #if int(timearray[2][:1])==int(day) and int(timearray[1])==int(month) and int(timearray[0])==int(year):
     #    hourdiff = abs(int(timearray[2][3:5])-hour)
     #    mindiff = abs(int(timearray[2][6:8])-minute)
@@ -71,11 +70,16 @@ try:
   #      yeardiff = abs(int(timearray[0])-int(year))
   #      greeting = tk.Label(text="This was taken "+str(yeardiff)+" ago",font = ('Times',24))
   #  greeting.pack()
-except:
-    pass
-l=Label()
-l.pack()
-os.system('cls' if os.name == 'nt' else 'clear')
+    except:
+        pass
+    l=Label()
+    l.pack()
+    os.system('cls' if os.name == 'nt' else 'clear')
+    count = find_all(dir_list)
+    print(count)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    speed = 900
+    x=1
 print("Checking files...")
 #find number of files that will be in slideshow
 
@@ -88,13 +92,8 @@ def find_all(dir_list):
             pass
     return counter
 
-count = find_all(dir_list)
-print(count)
-os.system('cls' if os.name == 'nt' else 'clear')
 #speed = int(input("How quickly would you like to run the photos? (out of 10 from fast to slow)"))
 #speed = speed*250
-speed = 900
-x=1
 def move():
     global x
     if x == count+1:
@@ -104,6 +103,7 @@ def move():
     x = x+1
     win.after(800+speed, move)  
 # calling the function
+run()
 i=0
 while True:
     if i%5 == 0:
@@ -113,6 +113,7 @@ while True:
         time.sleep(0.05)
         gpout = subprocess.Popen("stanislaus")
         gpout1=gpout.wait()
+        run()
     move()
     win.mainloop()
     i+=1
