28ba7f541a7a11ae02e52044340ac0aefb2013a1
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2024-03-19 21:59:51 +0000

    .

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 def config():
     
-    config = open("Slideshow_Config.txt",'r')
+    config = open("photobooth_config.txt",'r')
     config_array = config.readlines()
-    VERBOSE= config_array[4].split(' ')[-1].lower().strip("\n")
-    wait_time = config_array[5].split(' ')[-1].lower().strip("\n")
-    run_time = config_array[6].split(' ')[-1].lower().strip("\n")
-    fixed_dim = config_array[7].split(' ')[-1].lower().strip("\n")
-    Hostname = config_array[8].split(' ')[-1].lower().strip("\n")
-    name_code = config_array[9].split(' ')[-3].upper().strip("\n")
-    return VERBOSE,int(wait_time),run_time,fixed_dim,Hostname,name_code
+    VERBOSE= config_array[18].split(' ')[-1].lower().strip("\n")
+    wait_time = config_array[19].split(' ')[-1].lower().strip("\n")
+    run_time = config_array[20].split(' ')[-1].lower().strip("\n")
+    fixed_dim = config_array[21].split(' ')[-1].lower().strip("\n")
+    Hostname = config_array[22].split(' ')[-1].lower().strip("\n")
+    Venue = config_array[6].split(' ')[-2].upper().strip("\n")
+    return VERBOSE,int(wait_time),run_time,fixed_dim,Hostname,Venue
